e0de1f0b33529331730941086c61db67092bbda7
Rameez Khan <rameez@khan.com>
2023-03-23 15:36:07 +0500

    Home News UI Created

---
+++ app/src/main/java/com/example/newsbank/fragments/HomeNewsFragment.kt
@@ -0,0 +1,19 @@
+package com.example.newsbank.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.newsbank.R
+
+class HomeNewsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home_news, container, false)
+    }
+}
